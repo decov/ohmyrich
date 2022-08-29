142ecff9a202d7c58d165f1b24be6e5e599b3077
furs <90540827+furstamp@users.noreply.github.com>
2022-08-29 19:17:40 -0300

    Create unrealstatus.Build.cs

---
+++ discord-rpc/examples/unrealstatus/Source/unrealstatus/unrealstatus.Build.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+
+public class unrealstatus : ModuleRules
+{
+	public unrealstatus(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+
+		PrivateDependencyModuleNames.AddRange(new string[] {  });
+	}
+}
