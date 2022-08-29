ac5a506212fc2d1d0cdea316c282047ddab40473
furs <90540827+furstamp@users.noreply.github.com>
2022-08-29 19:15:03 -0300

    Create unrealstatusGameModeBase.h

---
+++ discord-rpc/examples/unrealstatus/unrealstatus/unrealstatusGameModeBase.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "unrealstatusGameModeBase.generated.h"
+
+UCLASS()
+class UNREALSTATUS_API AunrealstatusGameModeBase : public AGameModeBase {
+    GENERATED_BODY()
+};
